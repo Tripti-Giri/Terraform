4812d0bc0a9dc83a76e1cd324db44eaf526ece98
Tripti Giri <triptigiri872gmail.com>
2026-06-20 02:30:37 +0530

    NAT gateway for private subnet outbound traffic

--- Day_4_vpc/main.tf
+++ Day_4_vpc/main.tf
@@ -1,3 +1,8 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# --------VPC----------------------------------------------
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
 
@@ -6,6 +11,7 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+#------------IGW------------------------------------------
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 
@@ -14,10 +20,32 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+#----------ELASTIC IP FOR NAT------------------------------
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+#-----------NAT GATEWAY (lives in public subnet)----------
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "main-nat"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+#-------PUBLIC SUBNET-------------------------------------
 resource "aws_subnet" "public" {
-  vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 
   tags = {
@@ -25,10 +53,29 @@ resource "aws_subnet" "public" {
   }
 }
 
-resource "aws_subnet" "private" {
+resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.vpc.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "ap-south-1a"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+#------PRIVATE SUBNET--------------------------------------
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
 
   tags = {
@@ -39,12 +86,17 @@ resource "aws_subnet" "private" {
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.vpc.id
 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
   tags = {
     Name = "private-rt"
   }
 }
 
-resource "aws_route_table_association" "private_association" {
-  subnet_id = aws_subnet.private.id
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private_rt.id
 }
